2aa250ff8fbbe11d3dbf8eb2427df05f202ceb47
Ricardo Silva <ricardo.silva@3decide.com>
2022-02-07 00:17:17 +0000

    fixes use of port environent variable

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const port = process.env.SERVER_PORT;
 require('dotenv/config');
 
 const countriesRoute = require('./routes/countries');
@@ -21,6 +20,6 @@ app.get('/', (req, res) => {
   res.send('/');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server running on port ${process.env.SERVER_PORT}`);
 });
